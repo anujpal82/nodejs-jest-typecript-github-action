e1ec07d49d55470f9a3626863b28687d72b0c61a
Anuj Pal <anujpal631998@gmail.com>
2025-03-04 22:17:49 +0530

    fix: update test expectation to match new data value

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 describe('Sample Test File', () => {
     it('should return data', async () => {
-        const data = 'data';
+        const data = 'data1';
         expect(data).toEqual('data');
     });
 });
